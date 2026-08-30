e7f46af3f9525fbb98a5bd727da8a60fb2757552
Tausif Alam <123tausifalam123@gmail.com>
2026-08-30 23:09:53 +0530

    userhome page mapping

--- src/main/java/com/tausif/controller/FrontController.java
+++ src/main/java/com/tausif/controller/FrontController.java
@@ -7,6 +7,7 @@ import com.tausif.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,11 @@ public class FrontController {
             m.addAttribute("msg", "User Already Exist! 😒");
         }
         return "signup";
+    }
+
+    @GetMapping("/useerHome")
+    public String userhome(){
+        return "userHome";
     }
 
 }
