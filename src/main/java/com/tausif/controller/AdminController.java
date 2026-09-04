1334cc49ad4a83167b5cd5a78ac35b264cece2fa
Tausif Alam <123tausifalam123@gmail.com>
2026-09-04 20:09:37 +0530

    add feature check Admin_Login

--- src/main/java/com/tausif/controller/AdminController.java
+++ src/main/java/com/tausif/controller/AdminController.java
@@ -8,10 +8,11 @@ import com.tausif.service.ComplaintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -38,6 +39,19 @@ public class AdminController {
             m.addAttribute("msg", "User Registered Successfully!");
         }
         return "login";
+    }
+
+    @PostMapping("/checkLogin")
+    public String checkLogin(@RequestParam String email, @RequestParam String password, RedirectAttributes ra){
+        Admin a = adminService.checkLogin(email, password);
+        if(a == null){
+            ra.addFlashAttribute("msg", "Wrong Credentials!");
+        }else{
+            LocalDateTime time = LocalDateTime.now();
+            String greet = "Logged in at " + time;
+            ra.addFlashAttribute("msg", greet);
+        }
+        return "redirect:/adminHome";
     }
 
     @GetMapping("/allComplaints")
--- src/main/java/com/tausif/service/AdminService.java
+++ src/main/java/com/tausif/service/AdminService.java
@@ -3,8 +3,13 @@ package com.tausif.service;
 
 import com.tausif.beans.Admin;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AdminService {
@@ -16,6 +21,15 @@ public class AdminService {
 
     public Admin createAdmin(Admin admin) {
         Admin a = restTemplate.postForObject(adminUrl+"/adminRegister", admin, Admin.class);
+        return a;
+    }
+
+    public Admin checkLogin(String email, String password) {
+        Map<String, Object> cred = new HashMap<>();
+        cred.put("email", email);
+        cred.put("password", password);
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<Map<String, Object>>(cred);
+        Admin a = restTemplate.postForObject(adminUrl+"/login", requestEntity, Admin.class);
         return a;
     }
 }
